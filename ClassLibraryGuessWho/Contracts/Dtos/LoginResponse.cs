c714e5b0a2fd2620bfff3056a4d6f9332c2ac4a2
AlejandroMartinez5 <martinezramirezalejandro02@gmail.com>
2025-10-11 16:15:44 -0600

    Se desarrollo el servicio de login con sus DTOS y sus clases request y response correspondientes (#2)

---
+++ ClassLibraryGuessWho/Contracts/Dtos/LoginResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace ClassLibraryGuessWho.Contracts.Dtos
+{
+    [DataContract]
+    public class LoginResponse
+    {
+        [DataMember(IsRequired = true)]
+        public string User { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public string Password { get; set; }
+    }
+}
---
+++ WcfServiceLibraryGuessWho/Services/LoginService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GuessWho.Services.WCF.Security;
+using ClassLibraryGuessWho.Contracts.Dtos;
+using ClassLibraryGuessWho.Contracts.Services;
+using ClassLibraryGuessWho.Data;
+
+namespace WcfServiceLibraryGuessWho.Services
+{
+    public class LoginService : ILoginService
+    {
+        public LoginResponse LoginUser(LoginRequest request)
+        {
+            using (var db = new GuessWhoDB())
+            {
+                var account = db.ACCOUNT
+                    .Where(a => a.EMAIL == request.User)
+                    .Select(a => new { a.PASSWORD, a.USERID })
+                    .FirstOrDefault();
+
+                if (account == null || !PasswordHasher.Verify(request.Password, account.PASSWORD))
+                    return null;
+
+                var profile = db.USER_PROFILE.FirstOrDefault(u => u.USERID == account.USERID);
+
+                return new LoginResponse
+                {
+                    User = account.USERID.ToString(),
+                    Password = profile?.DISPLAYNAME
+                };
+            }
+        }
+
+    }
+}
