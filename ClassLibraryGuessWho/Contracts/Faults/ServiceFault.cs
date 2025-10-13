bff7b8405e274da91d09aecdc63283fb6387ede6
tuzc0 <claudiojtz1402@gmail.com>
2025-10-13 15:55:07 -0600

    feat(server): RegisterUserWPF – agrega Faults/ y Helpers/, actualiza IUserService y UserService, csproj (#5)

---
+++ ClassLibraryGuessWho/Contracts/Faults/ServiceFault.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+[DataContract]
+public sealed class ServiceFault
+{
+    [DataMember(Order = 1)] public string Code { get; set; }
+    [DataMember(Order = 2)] public string Message { get; set; }
+    [DataMember(Order = 3)] public string CorrelationId { get; set; }
+}
