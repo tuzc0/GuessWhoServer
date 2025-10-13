6d2a4531263e6797ea1420ab3c78568087580e31
AlejandroMartinez5 <martinezramirezalejandro02@gmail.com>
2025-10-13 14:02:47 -0600

    Fix in login service implementation and partial implementation of profile service

---
+++ ClassLibraryGuessWho/Contracts/Services/IProfileService.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibraryGuessWho.Contracts.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryGuessWho.Contracts.Services
+{
+    [ServiceContract]
+    internal interface IProfileService
+    {
+        [OperationContract]
+        ProfileResponse Profile(ProfileRequest request);
+    }
+}
